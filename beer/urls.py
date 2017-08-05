edd5021dcec766329295b120b62cffa828cef13f
jjjjjuniiiii <hotty226@naver.ocm>
2017-08-05 15:48:50 +0900

    Bootstrap applied

--- beer/urls.py
+++ beer/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^list/$', views.list, name='list'),
-    url(r'^write/$', views.list, name='write'),
-    url(r'^login/$', views.list, name='login'),
+url(r'^$', views.index, name='index'),
+
+url(r'^index/$', views.index, name='index'),
+
+url(r'^about/$', views.about, name='about'),
+url(r'^beer-listing/$', views.beer_listing, name='beer_listing'),
+url(r'^beer-details/$', views.beer_details, name='beer_details'),
+url(r'^contact/$', views.contact, name='contact'),
+url(r'^write/$', views.write, name='write'),
+url(r'^login/$', views.login, name='login'),
 ]
--- beer/views.py
+++ beer/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Beer
 
 # Create your views here.
 def index(request):
-	return render(request, 'beer/index.html',{})
+	beers = Beer.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'beer/index.html', {'beers': beers})
+	
+def about(request):
+	return render(request, 'beer/about.html',{})
 
-def list(request):
-	return render(request, 'beer/list.html',{})
+def beer_listing(request):
+	return render(request, 'beer/beer-listing.html',{})
+
+def beer_details(request):
+	return render(request, 'beer/beer-details.html',{})
+
+def contact(request):
+	return render(request, 'beer/contact.html',{})
 
 def write(request):
 	return render(request, 'beer/write.html',{})
